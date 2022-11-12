301d8af14f691488df84970885692f24feefe41a
azhebko <azhebko87@gmail.com>
2022-11-13 00:17:00 +0600

    first java app hello world example

---
+++ src/main/java/oop/y2022/helloworld/Main.java
@@ -0,0 +1,11 @@
+package oop.y2022.helloworld;
+
+import java.lang.String;
+
+/** Первое приложение на Java: вывод 'Hello, world!' в консоль */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
